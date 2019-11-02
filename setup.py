49bbade2294ce8bbce8c6daba42e0e8c45611593
CO18326 <42867901+CO18326@users.noreply.github.com>
2019-11-03 04:11:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,17 @@ setup(
   version = '0.1',      
   license='MIT',        
   description = 'A new version of python range',   
+  long_description='rangev2 is next version of popular range class of python, which enables you to produce lists even by using *,/,// and % operators along with +,- operators'+
+  '\n The syntax is nearly similar to the privious versions'+
+  '\nLittle modification is that step (third parametr) is now string containing operand and operator'+
+  "\nnew_range(8,100,'*9')"+
+'\n[8,72]'+
+  '\nprint(new_range(begin,end,step)) will now print the entire list'+
+  '\nexample'+ 
+"\nprint(new_range(100,1,'//3')"+
+"\n[100, 33, 11, 3]"+
+   '\nthe list generated can also be accessed via list argument'+ 
+  '\nexample : new_range(6).list[0,1,2,3,4,5]',
   author = 'Ishan Gambhir',                   
   author_email = 'ishugambhir2001@gmail.com',      
   url = 'https://github.com/co18326/rangev2',   
